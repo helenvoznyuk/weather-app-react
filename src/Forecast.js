23b429a3768dbc9fb681438989af7213a94b8ed9
Helen <vznkhelen@gmail.com>
2022-11-02 21:45:00 +0200

    Implemented daily forecast for default city

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,82 +1,48 @@
-import React from "react";
+import React, { useState } from "react";
 import "./Forecast.css";
 import "./App.css";
+import ForecastDay from "./ForecastDay";
 import "bootstrap/dist/css/bootstrap.min.css";
 import axios from "axios";
 
 export default function Forecast(props) {
+  let [loaded, setLoaded] = useState(false);
+  let [forecast, setForecast] = useState(null);
+
   function handleResponse(response) {
-    console.log(response.data);
+    setForecast(response.data.daily);
+    setLoaded(true);
   }
 
-  let apiKey = "dd339t2a63a34246b1fbc1f12o2030e0";
-  let longitude = props.coordinates.longitude;
-  let latitude = props.coordinates.latitude;
-  let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
-  axios.get(apiUrl).then(handleResponse);
-
-  return (
-    <div className="Forecast">
-      <div className="card border-secondary mb-3 days-card">
-        <div className="card-body">
-          <div className="row">
-            <div className="card day-one" id="day-one">
-              <div className="card-body">
-                <span className="monday">Monday</span>
-                <div className="forecast-icon">
-                  <i className="fa-solid fa-cloud-sun icon-cloud-sun"></i>
-                </div>
-                <span className="temp-max temperature-monday">21° | </span>
-                <span className="temp-min temperature-monday">9°</span>
-              </div>
-            </div>
-
-            <div className="card day-one" id="day-two">
-              <div className="card-body">
-                <span className="monday">Monday</span>
-                <div className="forecast-icon">
-                  <i className="fa-solid fa-cloud-sun icon-cloud-sun"></i>
-                </div>
-                <span className="temp-max temperature-monday">21° | </span>
-                <span className="temp-min temperature-monday">9°</span>
-              </div>
-            </div>
-
-            <div className="card day-one" id="day-three">
-              <div className="card-body">
-                <span className="monday">Monday</span>
-                <div className="forecast-icon">
-                  <i className="fa-solid fa-cloud-sun icon-cloud-sun"></i>
-                </div>
-                <span className="temp-max temperature-monday">21° | </span>
-                <span className="temp-min temperature-monday">9°</span>
-              </div>
-            </div>
-
-            <div className="card day-one" id="day-four">
-              <div className="card-body">
-                <span className="monday">Monday</span>
-                <div className="forecast-icon">
-                  <i className="fa-solid fa-cloud-sun icon-cloud-sun"></i>
-                </div>
-                <span className="temp-max temperature-monday">21° | </span>
-                <span className="temp-min temperature-monday">9°</span>
-              </div>
-            </div>
-
-            <div className="card day-one" id="day-five">
-              <div className="card-body">
-                <span className="monday">Monday</span>
-                <div className="forecast-icon">
-                  <i className="fa-solid fa-cloud-sun icon-cloud-sun"></i>
-                </div>
-                <span className="temp-max temperature-monday">21° | </span>
-                <span className="temp-min temperature-monday">9°</span>
-              </div>
+  if (loaded) {
+    return (
+      <div className="Forecast">
+        <div className="card border-secondary mb-3 days-card">
+          <div className="card-body">
+            <div className="row">
+              {forecast.map(function (dailyForecast, index) {
+                if (index < 5) {
+                  return (
+                    <div className="card day-one" id="day-one" key={index}>
+                      <div className="card-body">
+                        <ForecastDay data={dailyForecast} />
+                      </div>
+                    </div>
+                  );
+                }
+              })}
             </div>
           </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  } else {
+    let apiKey = "dd339t2a63a34246b1fbc1f12o2030e0";
+    let longitude = props.coordinates.longitude;
+    let latitude = props.coordinates.latitude;
+    let apiUrl = `https://api.shecodes.io/weather/v1/forecast?lon=${longitude}&lat=${latitude}&key=${apiKey}&units=metric`;
+    axios.get(apiUrl).then(handleResponse);
+
+    return "Loading...";
+  }
 }
---
+++ src/ForecastDay.js
@@ -0,0 +1,49 @@
+import React from "react";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./Forecast.css";
+import "./App.css";
+
+export default function ForecastDay(props) {
+  function maxTemperature() {
+    let temperature = Math.round(props.data.temperature.maximum);
+    return `${temperature}°`;
+  }
+
+  function minTemperature() {
+    let temperature = Math.round(props.data.temperature.minimum);
+    return `${temperature}°`;
+  }
+
+  function day() {
+    let date = new Date(props.data.time * 1000);
+    let day = date.getDay();
+
+    let days = [
+      "Sunday",
+      "Monday",
+      "Tuesday",
+      "Wednesday",
+      "Thursday",
+      "Friday",
+      "Saturday",
+    ];
+
+    return days[day];
+  }
+
+  return (
+    <div className="ForecastDay">
+      <span className="monday">{day()}</span>
+      <div className="forecast-icon">
+        <img
+          src={props.data.condition.icon_url}
+          id="icon"
+          alt={props.data.condition.icon}
+          className="fa-solid fa-cloud-sun icon-cloud-sun"
+        />
+      </div>
+      <span className="temp-max temperature-monday">{maxTemperature()} | </span>
+      <span className="temp-min temperature-monday">{minTemperature()}</span>
+    </div>
+  );
+}
