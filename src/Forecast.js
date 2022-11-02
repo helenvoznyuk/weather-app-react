03073d859ecb42248361789b4521ccb71b9246ab
Helen <vznkhelen@gmail.com>
2022-11-02 22:15:49 +0200

    Implemented daily forecast for searched cities

--- src/Forecast.js
+++ src/Forecast.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import "./Forecast.css";
 import "./App.css";
 import ForecastDay from "./ForecastDay";
@@ -8,6 +8,10 @@ import axios from "axios";
 export default function Forecast(props) {
   let [loaded, setLoaded] = useState(false);
   let [forecast, setForecast] = useState(null);
+
+  useEffect(() => {
+    setLoaded(false);
+  }, [props.coordinates]);
 
   function handleResponse(response) {
     setForecast(response.data.daily);
